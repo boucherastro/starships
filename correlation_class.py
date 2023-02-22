Can now scale the CCF map with a robust standard deviation. Added the possibility to use only a single sequence when doing a retrieval (with do_tr = [1], for example). Corrected a few things in the computation of TP profiles

correlation_class.py
@@ -94,7 +94,8 @@ class Correlations():
 
 
     def calc_logl(self, data_obj, icorr=None, orders=None, index=None, 
-                  N=None, nolog=None, alpha=None, inj_alpha='ones', kind_obj='seq', **kwargs):  
+                  N=None, nolog=None, alpha=None, inj_alpha='ones', kind_obj='seq',
+                  std_robust=True, **kwargs):
         
         if orders is None:
             orders = list(np.arange(49))
@@ -124,9 +125,14 @@ class Correlations():
                     except KeyError:
                         alpha = data_obj['trall_alpha_frac']
 
+        if std_robust:
+            std_data = np.nanmedian(np.abs(self.data - np.nanmedian(self.data, axis=3)[:, :,:,None]), axis=3)[:, :,:,None] / 0.6745
+            data = self.data/std_data
+        else:
+            data = self.data
 
-        self.logl0 = np.nansum( self.data[:, orders], axis=1)
-        self.logl = corr.sum_logl(self.data, icorr, orders, N, alpha=alpha, 
+        self.logl0 = np.nansum( data[:, orders], axis=1)
+        self.logl = corr.sum_logl( data, icorr, orders, N, alpha=alpha,
                                   axis=0, del_idx=index, nolog=nolog, **kwargs)
         
     def calc_logl_snr(self, n_pca=None, **kwargs):
@@ -1179,8 +1185,11 @@ class Correlations():
             plt.ylabel(r'Orbital Phase', fontsize=16)
             plt.axhline(tr.phase[tr.iIn[0]], color='white',linestyle='--')
             plt.axhline(tr.phase[tr.iIn[-1]], color='white',linestyle='--')
-            plt.axhline(tr.phase[tr.total[0]], color='white', linestyle=':')
-            plt.axhline(tr.phase[tr.total[-1]], color='white', linestyle=':')
+            try:
+                plt.axhline(tr.phase[tr.total[0]], color='white', linestyle=':')
+                plt.axhline(tr.phase[tr.total[-1]], color='white', linestyle=':')
+            except AttributeError:
+                pass
             plt.axvline(0, color='black',linestyle=':', alpha=0.7)
             fig2.tight_layout()
             fig2.savefig(path_fig +'fig_sum_CCF2D_'+fig_name+'.pdf')#, rasterize=True)

planet_obs.py
@@ -1418,7 +1418,7 @@ def merge_velocity(tr_merge, list_tr, merge_tr_idx):
     tr_merge.vr = np.concatenate([list_tr[str(tr_i)].vr for tr_i in merge_tr_idx])
     tr_merge.RV_const = np.concatenate([list_tr[str(tr_i)].RV_const* \
                                        np.ones((list_tr[str(tr_i)].n_spec)) for tr_i in merge_tr_idx])
-    
+    tr_merge.Kp = list_tr[str(tr_i)].Kp
 
 
 def split_transits(obs_obj, transit_tag, mid_idx, 
@@ -2020,15 +2020,17 @@ def load_sequences(filename, do_tr, path='', load_all=False):
     filename = Path(filename)
     path = Path(path)
 
-    out_filename = Path(f'{filename.name}_data_info.npz')
-    print('Reading:', path / out_filename)
-    data_info_file = np.load(path / out_filename)
-    data_info = {}
+    if len(do_tr) > 1 :
+        out_filename = Path(f'{filename.name}_data_info.npz')
+        print('Reading:', path / out_filename)
+        data_info_file = np.load(path / out_filename)
+        data_info = {}
 
-    data_info['trall_alpha_frac'] = data_info_file['trall_alpha_frac']
-    data_info['trall_icorr'] = data_info_file['trall_icorr']
-    data_info['trall_N'] = data_info_file['trall_N']
-    data_info['bad_indexs'] = data_info_file['bad_indexs']
+        data_info['trall_alpha_frac'] = data_info_file['trall_alpha_frac']
+        data_info['trall_icorr'] = data_info_file['trall_icorr']
+        data_info['trall_N'] = data_info_file['trall_N']
+        data_info['bad_indexs'] = data_info_file['bad_indexs']
+
 
     data_trs = {}
     #     flux = []
@@ -2039,6 +2041,13 @@ def load_sequences(filename, do_tr, path='', load_all=False):
         out_filename = Path(f'{filename.name}_data_trs_{i_tr}.npz')
         print('Reading:', path / out_filename)
         data_tr = np.load(path / out_filename)
+
+        if len(do_tr) <= 1:
+            data_info = {}
+            data_info['trall_alpha_frac'] = data_tr['alpha_frac']
+            data_info['trall_icorr'] = data_tr['icorr']
+            data_info['trall_N'] = data_tr['N']
+            data_info['bad_indexs'] = data_tr['bad_indexs']
 
         pca=PCA(data_tr['n_components_'])
         pca.components_ = data_tr['components_']