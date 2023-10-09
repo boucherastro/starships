47fae46c439f037b162b616b1c45cb4552e9d127
AntoineDarveau <adb@astro.umontreal.ca>
2023-10-09 17:17:05 -0400

    More restrictive versions of packages.

--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'astropy<=5.1',
+        'astropy==5.1',
         'batman-package==2.4.8',
         'corner<=2.2.1',
         'emcee<=3.1.3',
         'exo-k<=1.2.0',
         'exofile>=0.2.2',
-        'h5py<=3.7',
+        'h5py==3.7',
         'matplotlib<3.7',
-        'numpy<=1.23.0',
+        'numpy==1.23.0',
         'scikit-learn==1.0.2',
         'arviz==0.13.0',
         'astroquery==0.4.6',
         'molmass',
+        'arviz==0.13.0',
+        'netcdf4==1.5.*',
+        'PyAstronomy==0.18.0'
     ],
     entry_points={
         'console_scripts': [
