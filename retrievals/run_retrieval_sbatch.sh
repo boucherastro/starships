bc1a5d9ea2d4ba56cbd76cbef5e1ab52d5f75c17
AntoineDarveau <adb@astro.umontreal.ca>
2024-06-20 14:02:32 -0400

    Example sbatch script

--- retrievals/run_retrieval_sbatch.sh
+++ retrievals/run_retrieval_sbatch.sh
@@ -5,14 +5,20 @@
 #SBATCH --mem=50G
 #SBATCH --time=3-00:00
 #SBATCH --job-name W33_d2
-#SBATCH --output=/home/adb/scratch/sbatch_outputs/out_sbatch_%j.txt
+#SBATCH --output=/home/adb/scratch/sbatch_outputs/out_sbatch_%A_%a.txt
 #SBATCH --mail-type=FAIL
+
+# For burnin, use slurms array option
+# Ex: #SBATCH --array=1-10
+
+# Note regarding slurm array:
+#    The default behaviour of run_starships_retrieval is to assume that if slurm array is used,
+#    the retrieval is in its burnin phase. Therefore, the retrieval is launched as many times
+#    as the number of jobs in the array (1 to 10 in this example, so 10 times).
+#    This is useful to explore a wider parameter space, using 10 times more walker in this case.
+#    This behaviour is controlled by the flag `slurm_array_behaviour` in the yaml file.
 
 source ~/.venvs/starships_env_39/bin/activate
 
 echo "Starting python code..."
-python retrieval_WASP-33b_JR_guillot_chem_eq_copy.py yaml_file=retrieval_inputs_example_wasp33.yaml
-
-# Burnin (use with >>> #SBATCH --array=1-10 for example)
-# echo "Starting python code with SLURM_ARRAY_TASK_ID = $SLURM_ARRAY_TASK_ID"
-# python retrieval_WASP-33b_JR_guillot_chem_eq_copy.py idx_file=$SLURM_ARRAY_TASK_ID
+run_starships_retrieval yaml_file=retrieval_inputs_example_wasp33.yaml
