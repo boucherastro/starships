d884f0f93d97d06545a302ed10a4443d2b114e42
Olivia Pereira <oliviapereira2002@gmail.com>
2024-09-06 15:33:58 -0400

    reorganizing for loops

--- pipeline/run_pipe.py
+++ pipeline/run_pipe.py
@@ -2,7 +2,7 @@ import numpy as np
 import yaml
 from pathlib import Path
 import argparse
-import os
+import matplotlib.pyplot as plt
 
 from multiprocessing import Pool
 from itertools import product
@@ -12,52 +12,93 @@ import pipeline.make_model as mod
 import pipeline.correlations as corr
 import pipeline.split_nights as split
 
-def main_loop(mask_tellu, mask_wings, n_pc, mol, config_dict, planet, obs, scratch_dir, out_dir, visit_name, wave_mod, mod_spec):
-    
+'''--------------------------------------Helper Functions----------------------------------------'''
+def main_loop(mask_tellu, mask_wings, n_pc, mol, config_dict, planet, obs, dirs_dict, 
+              visit_name, wave_mod, mod_spec):
+    ''' Main loop for the pipeline. '''
     naming_params = {'molecule': mol, 'n_pc': n_pc, 'mask_tellu': mask_tellu, 'mask_wings': mask_wings}
     print('CURRENT PARAMETERS:', naming_params)
 
     # reducing the data
-    red_steps_dir = out_dir / Path('Results') / Path('Reduction_steps')
-    reduc = red.reduce_data(config_dict, planet, obs, scratch_dir, red_steps_dir, n_pc, mask_tellu, mask_wings, visit_name)
+    reduc = red.reduce_data(config_dict, planet, obs, dirs_dict['scratch_dir'], 
+                            dirs_dict['red_steps_dir'], n_pc, mask_tellu, mask_wings, visit_name)
 
     # perfoming classic ccf (translates model)
-    classic_ccf_dir = out_dir / Path('Results') / Path('CCF_classic')
-    classic_ccf_dir.mkdir(parents=True, exist_ok=True)
     nametag = f'_{visit_name}_{mol}_maskwings{mask_wings*100:n}_masktellu{mask_tellu*100:n}_pc{n_pc}'
-    corr.classic_ccf(config_dict, reduc, wave_mod, mod_spec, classic_ccf_dir, nametag) 
+    corr.classic_ccf(config_dict, reduc, wave_mod, mod_spec, dirs_dict['classic_ccf_dir'], nametag) 
 
     # performing injected ccf 
-    injected_ccf_dir = out_dir / Path('Results') / Path('CCF_injected')
-    corr.perform_ccf(config_dict, reduc, mol, wave_mod, mod_spec, n_pc, mask_tellu, mask_wings, scratch_dir, injected_ccf_dir, visit_name)
+    corr.perform_ccf(config_dict, reduc, mol, wave_mod, mod_spec, n_pc, mask_tellu, mask_wings, 
+                     dirs_dict['scratch_dir'], dirs_dict['injected_ccf_dir'], visit_name)
 
     # combining visits
-    if len(config_dict['visit_name']) > 1:
-        if ([mask_tellu, mask_wings, n_pc] == config_dict['night_params']) and (visit_name == config_dict['visit_name'][-1]):
-            comb_dir_dict = red.set_save_location(config_dict['pl_name'], 'combined', config_dict['reduction'], config_dict['instrument'])
+    if (len(config_dict['visit_name']) > 1) and (visit_name == config_dict['visit_name'][-1]):
+        if ([mask_tellu, mask_wings, n_pc] == config_dict['night_params']):
+            comb_dir_dict = red.set_save_location(config_dict['pl_name'], 'combined', 
+                                                  config_dict['reduction'], config_dict['instrument'])
             corr.combined_visits_ccf(planet, mol, wave_mod, mod_spec, comb_dir_dict, config_dict)
 
 def main_loop_wrapper(*args):
+    ''' Wrapper function to pass multiple arguments to the main loop. '''
     *iterable_args, kwargs = args
     return main_loop(*iterable_args, **kwargs)
 
-## running the pipeline
-def run_pipe(config_filepath, run_name):
+def multi_param_plots(config_dict, planet, mol, dirs_dict, visit_name):
+    ''' Function to create plots for all parameters. '''
+    # plots for each pc
+    for mask_tellu in config_dict['mask_tellu']:
+        for mask_wings in config_dict['mask_wings']:
+            corr.plot_all_ccf(config_dict, mol, mask_tellu, mask_wings, dirs_dict['scratch_dir'], 
+                            visit_name, planet, id_pc0=None, order_indices=np.arange(75), 
+                            path_fig = dirs_dict)
 
+    # plots for each mask_wings 
+    for mask_tellu in config_dict['mask_tellu']:
+        for n_pc in config_dict['n_pc']:
+            # load all ccf map and reductions for (mask_tellu, n_pc) at each mask_wings
+            corr.plot_all_maskwings(config_dict, planet, mol, mask_tellu, n_pc, dirs_dict['scratch_dir'], 
+                                    visit_name, id_pc0=None, order_indices=np.arange(75), 
+                                    path_fig = str(dirs_dict['param_dir']) + '/')
+
+    # plots for each mask_tellu
+    for n_pc in config_dict['n_pc']:
+        for mask_wings in config_dict['mask_wings']:
+            corr.plot_all_masktellu(config_dict, planet, mol, mask_wings, n_pc, dirs_dict['scratch_dir'], 
+                                    visit_name, id_pc0=None, order_indices=np.arange(75), 
+                                    path_fig = str(dirs_dict['param_dir']) + '/')
+
+def pool_processing(config_dict, planet, obs, dirs_dict, visit_name, wave_mod, mod_spec, mol):
+    ''' Function to run the main loop in parallel using multiprocessing '''
+    iterables = product(config_dict['mask_tellu'], config_dict['mask_wings'], config_dict['n_pc'])
+
+    # Input n_processes to see how many iterables to run in parallel
+    n_processes = 4
+    with Pool(n_processes) as pool:
+        args = {
+            'mol': mol,
+            'config_dict': config_dict,
+            'planet': planet,
+            'obs': obs,
+            'dirs_dict': dirs_dict,
+            'visit_name': visit_name,
+            'wave_mod': wave_mod,
+            'mod_spec': mod_spec
+        }
+
+        # Create a list of all arguments including those from iterables
+        all_args = [(arg_tuple + (args,)) for arg_tuple in iterables]
+
+        # Use pool.starmap to pass multiple arguments
+        pool.starmap(main_loop_wrapper, all_args)
+           
+
+'''-------------------------------------Actual pipeline------------------------------------------'''
+def run_pipe(config_filepath, run_name):
     # unpack input parameters into config dictionary
     with open(config_filepath, 'r') as file:
         config_dict = yaml.safe_load(file)
 
     config_dict['reduction'] = run_name
-
-    # unpack the model input parameters, or load the existing model file
-    # with open(config_dict['input_model_file'], 'r') as file:
-    #     config_model = yaml.safe_load(file)
-
-    # # initialize all molecule abundances
-    # if config_model['species_vmr'] == {}:
-    #     for mol in config_model['line_opacities']:
-    #         config_model['species_vmr'][mol] = -99.0
 
     config_dict['obs_dir'] = Path.home() / Path(config_dict['obs_dir'])
 
@@ -67,15 +108,51 @@ def run_pipe(config_filepath, run_name):
     
     # loop over all visits
     for visit_name in config_dict['visit_name']:
-
-        dirs_dict = red.set_save_location(config_dict['pl_name'], visit_name, config_dict['reduction'], config_dict['instrument'])
-
         # creating the planet and observation objects
         if visit_name != 'combined':
             planet, obs = red.load_planet(config_dict, visit_name)
 
-        # check if input is an existing file or the config file for a new model
-        if config_dict['input_model_file'].endswith('.yaml'):
+        dirs_dict = red.set_save_location(config_dict['pl_name'], visit_name, 
+                                          config_dict['reduction'], config_dict['instrument'])
+
+        # if a list of existing models was given to test: 
+        if isinstance(config_dict['input_model_file'], list):
+            for file in config_dict['input_model_file']:
+                model = np.load(file)
+                wave_mod = model['wave_mod']
+                mod_spec = model['mod_spec']
+                mol = model['model_name']
+
+                # quick plot of the model
+                plt.figure()
+                plt.plot(wave_mod, mod_spec)
+                plt.title(f'{mol} model')
+                plt.xlabel('Wavelength (um)')
+                plt.ylabel('Flux')
+                plt.savefig(str(dirs_dict['out_dir']) + f'/{mol}_model.pdf')
+                
+                pool_processing(config_dict, planet, obs, dirs_dict, visit_name, wave_mod, mod_spec, mol)
+                multi_param_plots(config_dict, planet, mol, dirs_dict, visit_name)
+
+        # if a single model file was given to test
+        elif config_dict['input_model_file'].endswith('.npz'):
+            model = np.load(config_dict['input_model_file'])
+            wave_mod = model['wave_mod']
+            mod_spec = model['mod_spec']
+            mol = model['model_name']
+
+            plt.figure()
+            plt.plot(wave_mod, mod_spec)
+            plt.title(f'{mol} model')
+            plt.xlabel('Wavelength (um)')
+            plt.ylabel('Flux')
+            plt.savefig(str(dirs_dict['out_dir']) + f'/{mol}_model.pdf')
+                
+            pool_processing(config_dict, planet, obs, dirs_dict, visit_name, wave_mod, mod_spec, mol)
+            multi_param_plots(config_dict, planet, mol, dirs_dict, visit_name)
+
+        # else, we make our own model based on the input file
+        else: 
             with open(config_dict['input_model_file'], 'r') as file:
                 config_model = yaml.safe_load(file)
 
@@ -85,7 +162,8 @@ def run_pipe(config_filepath, run_name):
                     config_model['species_vmr'][mol] = -99.0
 
             # Start with model with all molecules
-            wave_mod, mod_spec, abundances, MMW, VMR = mod.make_model(config_model, planet, dirs_dict['out_dir'], config_dict)
+            wave_mod, mod_spec, abundances, MMW, VMR = mod.make_model(config_model, planet, 
+                                                                        dirs_dict['out_dir'], config_dict)
 
             if len(config_model['line_opacities']) == 1:
                 mol = config_model['line_opacities'][0]
@@ -93,134 +171,35 @@ def run_pipe(config_filepath, run_name):
 
             if visit_name == config_dict['visit_name'][0]:
                 mod.plot_model_components(config_model, planet, path_fig = str(dirs_dict['out_dir']))
-
-        # else, open the existing model file and save a plot of it
-        elif config_dict['input_model_file'].endswith('.npz'):
-            model = np.load(config_dict['input_model_file'])
-            wave_mod = model['wave_mod']
-            mod_spec = model['mod_spec']
-            mol = 'imported_model'
-
-# --------------------------------------------------------------------------------------------------------------------------------
-        iterables = product(config_dict['mask_tellu'], config_dict['mask_wings'], config_dict['n_pc'])
-
-        # Input n_processes to see how many iterables to run in parallel
-        n_processes = 4
-        with Pool(n_processes) as pool:
-            args = {
-                'mol': mol,
-                'config_dict': config_dict,
-                'planet': planet,
-                'obs': obs,
-                'scratch_dir': dirs_dict['scratch_dir'],
-                'out_dir': dirs_dict['out_dir'],
-                'visit_name': visit_name,
-                'wave_mod': wave_mod,
-                'mod_spec': mod_spec
-            }
-
-            # Create a list of all arguments including those from iterables
-            all_args = [(arg_tuple + (args,)) for arg_tuple in iterables]
-
-            # Use pool.starmap to pass multiple arguments
-            pool.starmap(main_loop_wrapper, all_args)
         
-        # include injection step here, need to change the dictionaries everything is saved to
-# --------------------------------------------------------------------------------------------------------------------------------
-        # plots for each pc
-        for mask_tellu in config_dict['mask_tellu']:
-            for mask_wings in config_dict['mask_wings']:
-                corr.plot_all_ccf(config_dict, mol, mask_tellu, mask_wings, dirs_dict['scratch_dir'], 
-                                visit_name, planet, id_pc0=None, order_indices=np.arange(75), 
-                                path_fig = dirs_dict)
+            pool_processing(config_dict, planet, obs, dirs_dict, visit_name, wave_mod, mod_spec, mol)
+            multi_param_plots(config_dict, planet, mol, dirs_dict, visit_name)
+            
+            # iterate over individual molecules if there are more than 1
+            if len(config_model['line_opacities']) > 1:
+                for single_mol in config_model['line_opacities']:
+                    # Copy input dict
+                    theta_dict_single = dict(config_model)
+                    abundances_subtracted = dict(abundances)
 
-        # plots for each mask_wings 
-        for mask_tellu in config_dict['mask_tellu']:
-            for n_pc in config_dict['n_pc']:
-                # load all ccf map and reductions for (mask_tellu, n_pc) at each mask_wings
-                corr.plot_all_maskwings(config_dict, planet, mol, mask_tellu, n_pc, 
-                                                            dirs_dict['scratch_dir'], visit_name, id_pc0=None, 
-                                                            order_indices=np.arange(75), path_fig = str(dirs_dict['param_dir']) + '/')
+                    # Need to be a list
+                    if not isinstance(single_mol, list): single_mol = [single_mol]
 
-        # plots for each mask_tellu
-        for n_pc in config_dict['n_pc']:
-            for mask_wings in config_dict['mask_wings']:
-                corr.plot_all_masktellu(config_dict, planet, mol, mask_wings, n_pc, 
-                                                            dirs_dict['scratch_dir'], visit_name, id_pc0=None, 
-                                                            order_indices=np.arange(75), path_fig = str(dirs_dict['param_dir']) + '/')
-        
+                    # set all other molecules to zero
+                    for mol in config_model['line_opacities']:
+                        if mol != single_mol[0]:
+                            linelist = config_model['linelist_names'][config_model['mode']][mol]
+                            abundances_subtracted[linelist] = abundances_subtracted[linelist]*0 + 1e-99
 
-        # iterate over individual molecules if there are more than 1
-        if config_dict['input_model_file'].endswith('.yaml') and len(config_model['line_opacities']) > 1:
-            for single_mol in config_model['line_opacities']:
-                
-                # Copy input dict
-                theta_dict_single = dict(config_model)
+                    theta_dict_single['chemical_equilibrium'] = False
+                    wave_mod, mod_spec, _, _, _ = mod.make_model(theta_dict_single, planet, out_dir = None, 
+                                                    abundances = abundances_subtracted,
+                                                    MMW = MMW, config_dict=config_dict)
 
-                abundances_subtracted = dict(abundances)
+                    pool_processing(config_dict, planet, obs, dirs_dict, visit_name, wave_mod, mod_spec, single_mol[0])
+                    multi_param_plots(config_dict, planet, single_mol[0], dirs_dict, visit_name)
 
-                # Need to be a list
-                if not isinstance(single_mol, list):
-                    single_mol = [single_mol]
-
-                # set all other molecules to zero
-                for mol in config_model['line_opacities']:
-                    if mol != single_mol[0]:
-                        linelist = config_model['linelist_names'][config_model['mode']][mol]
-                        abundances_subtracted[linelist] = abundances_subtracted[linelist] * 0 + 1e-99
-
-                theta_dict_single['chemical_equilibrium'] = False
-                wave_mod, mod_spec, _, _, _ = mod.make_model(theta_dict_single, planet, out_dir = None, 
-                                                abundances = abundances_subtracted,
-                                                MMW = MMW, config_dict=config_dict)
-
-# --------------------------------------------------------------------------------------------------------------------------------
-                iterables = product(config_dict['mask_tellu'], config_dict['mask_wings'], config_dict['n_pc'])
-
-                # Input n_processes to see how many iterables to run in parallel
-                n_processes = 4
-                with Pool(n_processes) as pool:
-                    args = {
-                        'mol': single_mol[0],
-                        'config_dict': config_dict,
-                        'planet': planet,
-                        'obs': obs,
-                        'scratch_dir': dirs_dict['scratch_dir'],
-                        'out_dir': dirs_dict['out_dir'],
-                        'visit_name': visit_name,
-                        'wave_mod': wave_mod,
-                        'mod_spec': mod_spec
-                    }
-
-                    # Create a list of all arguments including those from iterables
-                    all_args = [(arg_tuple + (args,)) for arg_tuple in iterables]
-
-                    # Use pool.starmap to pass multiple arguments
-                    pool.starmap(main_loop_wrapper, all_args)
-# --------------------------------------------------------------------------------------------------------------------------------
-
-                # plots for each pc
-                for mask_tellu in config_dict['mask_tellu']:
-                    for mask_wings in config_dict['mask_wings']:
-                        corr.plot_all_ccf(config_dict, single_mol[0], mask_tellu, mask_wings, dirs_dict['scratch_dir'], 
-                                        visit_name, planet, id_pc0=None, order_indices=np.arange(75), 
-                                        path_fig = dirs_dict)
-
-                # plots for each mask_wings 
-                for mask_tellu in config_dict['mask_tellu']:
-                    for n_pc in config_dict['n_pc']:
-                        # load all ccf map and reductions for (mask_tellu, n_pc) at each mask_wings
-                        corr.plot_all_maskwings(config_dict, planet, single_mol[0], mask_tellu, n_pc, 
-                                                                    dirs_dict['scratch_dir'], visit_name, id_pc0=None, 
-                                                                    order_indices=np.arange(75), path_fig = str(dirs_dict['param_dir']) + '/')
-
-                # plots for each mask_tellu
-                for n_pc in config_dict['n_pc']:
-                    for mask_wings in config_dict['mask_wings']:
-                        corr.plot_all_masktellu(config_dict, planet, single_mol[0], mask_wings, n_pc, 
-                                                                    dirs_dict['scratch_dir'], visit_name, id_pc0=None, 
-                                                                    order_indices=np.arange(75), path_fig = str(dirs_dict['param_dir']) + '/')
-        
+'''----------------------------------------------------------------------------------------------'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the pipeline with the given config files.')
     parser.add_argument('config_filepath', type=Path, help='Path to the config.yaml file.')
